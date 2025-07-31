10a953b13d8caa15a53bff62b45083a79e19bdc2
vansh-tandon <vanshtandon421@gmail.com>
2025-07-31 20:51:50 +0530

    added bottom navigation bar

--- app/src/main/java/com/example/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/example/rickandmorty/MainActivity.kt
@@ -3,10 +3,34 @@ package com.example.rickandmorty
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -16,10 +40,19 @@ import com.example.network.KtorClient
 import com.example.rickandmorty.screens.CharacterDetailsScreen
 import com.example.rickandmorty.screens.CharacterEpisodeScreen
 import com.example.rickandmorty.screens.HomeScreen
+import com.example.rickandmorty.ui.theme.RickAction
 import com.example.rickandmorty.ui.theme.RickAndMortyTheme
 import com.example.rickandmorty.ui.theme.RickPrimary
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
+
+sealed class NavDestination(val title: String, val route: String, val icon: ImageVector) {
+    object Home : NavDestination(title = "Home", route = "home_screen", icon = Icons.Filled.Home)
+    object Episodes :
+        NavDestination(title = "Episodes", route = "episodes", icon = Icons.Filled.PlayArrow)
+
+    object Search : NavDestination(title = "Search", route = "search", icon = Icons.Filled.Search)
+}
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -29,57 +62,124 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
 
             val navController = rememberNavController()
+            val items = listOf(
+                NavDestination.Home, NavDestination.Episodes, NavDestination.Search
+            )
+            var selectedIndex by remember { mutableIntStateOf(0) }
 
             RickAndMortyTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = RickPrimary
-                ) {
-                    NavHost(navController = navController, startDestination = "home_screen") {
-                        composable(route = "home_screen") {
-                            HomeScreen(onCharacterSelected = { characterId ->
-                                navController.navigate("character_details/$characterId")
-                            })
-                        }
-                        composable(
-                            route = "character_details/{characterId}",
-                            arguments = listOf(navArgument("characterId") {
-                                type = NavType.IntType
-                            })
-                        ) { backStackEntry ->
-                            val characterId: Int =
-                                backStackEntry.arguments?.getInt("characterId") ?: -1
-                            CharacterDetailsScreen(
-                                characterId = characterId,
-                                onEpisodeClicked = {
-                                    navController.navigate("character_episodes/$it")
-                                },
-                                onBackClicked = {
-                                    navController.navigateUp()
-                                }
-                            )
-                        }
-                        composable(
-                            route = "character_episodes/{characterId}",
-                            arguments = listOf(navArgument("characterId") {
-                                type = NavType.IntType
-                            })
-                        ) { backStackEntry ->
-                            val characterId: Int =
-                                backStackEntry.arguments?.getInt("characterId") ?: -1
-                            CharacterEpisodeScreen(
-                                characterId = characterId,
-                                ktorClient = ktorClient,
-                                onBackClicked = { navController.navigateUp() }
-                            )
+                Scaffold(
+                    bottomBar = {
+                        NavigationBar(
+                            containerColor = RickPrimary
+                        ) {
+                            items.forEachIndexed { index, screen ->
+                                NavigationBarItem(
+                                    icon = {
+                                        Icon(imageVector = screen.icon, contentDescription = null)
+                                    },
+                                    label = { Text(screen.title) },
+                                    selected = index == selectedIndex,
+                                    onClick = {
+                                        selectedIndex = index
+                                        navController.navigate(screen.route) {
+                                            popUpTo(navController.graph.findStartDestination().id) {
+                                                saveState = true
+                                            }
+                                            launchSingleTop = true
+                                            restoreState = true
+                                        }
+                                    },
+                                    colors = NavigationBarItemDefaults.colors(
+                                        selectedIconColor = RickAction,
+                                        selectedTextColor = RickAction,
+                                        indicatorColor = Color.Transparent
+                                    )
+                                )
+                            }
                         }
                     }
+                ) { innerPadding ->
+                    NavigationHost(
+                        navController = navController,
+                        ktorClient = ktorClient,
+                        innerPadding = innerPadding
+                    )
                 }
+            }
+        }
+    }
 
+    @Composable
+    private fun NavigationHost(
+        navController: NavHostController,
+        ktorClient: KtorClient,
+        innerPadding: PaddingValues
+    ) {
+        NavHost(
+            navController = navController,
+            startDestination = "home_screen",
+            modifier = Modifier
+                .background(color = RickPrimary)
+                .padding(innerPadding)
+        ) {
+            composable(route = "home_screen") {
+                HomeScreen(onCharacterSelected = { characterId ->
+                    navController.navigate("character_details/$characterId")
+                })
+            }
+            composable(
+                route = "character_details/{characterId}",
+                arguments = listOf(navArgument("characterId") {
+                    type = NavType.IntType
+                })
+            ) { backStackEntry ->
+                val characterId: Int =
+                    backStackEntry.arguments?.getInt("characterId") ?: -1
+                CharacterDetailsScreen(
+                    characterId = characterId,
+                    onEpisodeClicked = {
+                        navController.navigate("character_episodes/$it")
+                    },
+                    onBackClicked = {
+                        navController.navigateUp()
+                    }
+                )
+            }
+            composable(
+                route = "character_episodes/{characterId}",
+                arguments = listOf(navArgument("characterId") {
+                    type = NavType.IntType
+                })
+            ) { backStackEntry ->
+                val characterId: Int =
+                    backStackEntry.arguments?.getInt("characterId") ?: -1
+                CharacterEpisodeScreen(
+                    characterId = characterId,
+                    ktorClient = ktorClient,
+                    onBackClicked = { navController.navigateUp() }
+                )
+            }
+            composable(route = NavDestination.Episodes.route) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "Episodes", fontSize = 62.sp, color = Color.White)
+                }
+            }
+            composable(route = NavDestination.Search.route) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "Search", fontSize = 62.sp, color = Color.White)
+                }
             }
         }
     }
